58dd78796bb2c23773dcdfb0a3c35c8d54037227
avid-coder05 <tandiv15.bansal@gmail.com>
2024-03-09 11:35:13 +0530

    Debugging update - Check login behaviour

--- login.py
+++ login.py
@@ -2,9 +2,11 @@ credentials=[]
 
 def add_cred(data):
     credentials.append([data['username'], data['password']])
+    print(credentials)
     return True
 
 def check_cred(data):
+    print(credentials)
     for i in credentials:
         if(i[0]==data['username'] and i[1]==data['password']):
             return True
